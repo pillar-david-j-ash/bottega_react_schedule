046e52b32cf0354e8a3f2a65101490b8974b3753
David Ash <david.j.ash@accenture.com>
2020-05-21 14:44:44 -0400

    build library and schedule components and put them on the grid

---
+++ src/components/schedule/schedule.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Library extends Component {
+    render() {
+        return (
+            <div className="schedule">
+                <h1 className="schedule_library">My Schedule</h1>
+            </div>
+        )
+    }
+}
+
+export default Library
