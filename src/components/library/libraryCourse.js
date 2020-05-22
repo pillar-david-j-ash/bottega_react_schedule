ea06cba038d15473f075d639fefdae4fa354cc68
David Ash <bottegadash@gmail.com>
2020-05-21 22:14:01 -0400

    fixed grid and aded arrow component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Icon from "../icon";
+import Arrow from "./arrow";
 
 class LibraryCourse extends Component {
-    render() {
-        return (
-            <div className="library-course">
-                <label className="library-course_title">Problem Solving</label>
-                { Icon("fas fa-check",  "library-course_icon") }
-                {/* arrow component */}
-                {/* action button component */}
-                <div className="library-course_description">
-                    <label>Course Description</label>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ultrices aliquet sem, non aliquet ipsum ultrices in. Praesent molestie tellus ornare fermentum rutrum. Orci </p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="library-course">
+        <div className="library-course_title-arrow">
+          <label className="library-course_title">Problem Solving</label>
+          {Icon("fas fa-check", "library-course_icon")}
+        </div>
+        <Arrow className="library-course_arrow" />
+        {/* action button component */}
+        <div className="library-course_description">
+          <label>Course Description</label>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+            ultrices aliquet sem, non aliquet ipsum ultrices in. Praesent
+            molestie tellus ornare fermentum rutrum. Orci{" "}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LibraryCourse;
