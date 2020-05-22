2a51082f68bfcd05c56c45d6f123f82b6cb29bdd
David Ash <david.j.ash@accenture.com>
2020-05-22 09:11:12 -0400

    built schedule course component and placed on grid

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Gradient from "../gradient";
+import ScheduleCourse from "./scheduleCourse";
 
 class Library extends Component {
     render() {
         return (
             <div className="schedule">
                 <h1 className="schedule_library">My Schedule</h1>
+                <ScheduleCourse title={"Problem Solving"}/>
+                <ScheduleCourse title={"Problem Solving"}/>
+                <ScheduleCourse title={"Problem Solving"}/>
                 <Gradient/>
             </div>
         )
