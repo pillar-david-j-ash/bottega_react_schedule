6f1439dfbaab6e12291f8dfe6ddfe4db4b2973ff
David Ash <david.j.ash@accenture.com>
2020-05-26 20:01:20 -0400

    mapped course data over to schedule component to only render courses user is enrolled in

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Gradient from "../gradient";
 import ScheduleCourse from "./scheduleCourse";
 import ProgressTracker from "./progressTracker";
 
 class Library extends Component {
+
+    renderCourses() {
+        const data = this.props.courses
+
+        return data.map( (course, index) => {
+            if (course.enrolled) {
+                return <ScheduleCourse {...course} key={index}/>
+            }
+        })
+    }
+
     render() {
         return (
             <div className="schedule">
                 <h1 className="schedule_library">My Schedule</h1>
-                <ScheduleCourse title={"Problem Solving"}/>
-                <ScheduleCourse title={"Problem Solving"}/>
-                <ScheduleCourse title={"Problem Solving"}/>
+                { this.renderCourses() }
                 <Gradient/>
                 <ProgressTracker/>
             </div>
@@ -18,4 +29,10 @@ class Library extends Component {
     }
 }
 
-export default Library
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps)(Library)
